d22576927dda405496a1249da89e9d4326baec5e
K G Ashalatha <kgashalatha3@gmail.com>
2026-04-17 19:13:47 +0530

    Add files via upload

---
+++ Automorphicnumber.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class Automorphicnumber
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int square=n*n;
+int temp=n;
+boolean isAutomorphic=true;
+while(temp>0)
+     {
+        if(temp%10!=square%10)
+             {
+                isAutomorphic=false;
+                break;
+              }
+        temp/=10;
+         square/=10;
+       }   
+if(isAutomorphic)
+{
+System.out.println("automorphic number");
+}
+else
+{
+System.out.println("not automorphic number");
+}
+}
+}
+        
