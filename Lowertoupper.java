57de67b92f110bb323a6fd1b55c4490f0bf8f03e
K G Ashalatha <kgashalatha3@gmail.com>
2026-04-20 14:52:19 +0530

    Add files via upload

---
+++ Lowertoupper.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Lowertoupper
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String str=sc.nextLine();
+String result="";
+for(int i=0;i<str.length();i++)
+{
+char ch=str.charAt(i);
+if(ch>='a'&&ch<='z')
+{
+ch=(char)(ch-32);
+}
+result+=ch;
+}
+System.out.println(result);
+}
+}
---
+++ Palindromestring.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Palindromestring
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String str=sc.nextLine();
+int start=0;
+int end=str.length()-1;
+boolean isPalindrome=true;
+while(start<end)
+  {
+     if(str.charAt(start)!=str.charAt(end))
+     {
+     isPalindrome=false;
+     break;
+      }
+   start++;
+    end--;
+ }
+if(isPalindrome)
+System.out.println( "palindrome");
+else
+System.out.println( "not a palindrome");
+}
+}
